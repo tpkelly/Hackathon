d51ba97d88f0cf40da0499d75bb3e0cb5a3db1d6
Ian Lovell <ILovell@scottlogic.com>
2014-08-14 16:09:23 +0100

    Added original version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-scott-logic-hackathon
-=====================
+Scott Logic Hackathon 2014
+=======================
+
+This is the project required for the Scott Logic Hackathon 2014.
+Download as a zip and import into Eclipse.
---
+++ src/trading/TradingStrategy.java
@@ -0,0 +1,26 @@
+package trading;
+
+import game.DailyOutput;
+import tradingstrategy.BaseTradingStrategy;
+import dataobjects.DailyInput;
+import exceptions.InsufficientFundsException;
+import exceptions.InsufficientSharesException;
+
+public class TradingStrategy extends BaseTradingStrategy {
+
+	@Override
+	public DailyOutput makeDailyTrade(DailyInput input) throws InsufficientFundsException, InsufficientSharesException {
+		//use the trading manager to make trades based on input
+		
+		
+		DailyOutput output;
+		if (input.getDay() % 2 == 0) {
+			output = tradingManager.buyMaxNumberOfShares(input);
+		} else {
+			output = tradingManager.sellAllShares(input);
+		}
+		
+		return output;
+	}
+
+}
